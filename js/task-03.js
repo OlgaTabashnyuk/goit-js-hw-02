a29304c180e6c1656b7f3042d89b21980fc9d991
Olga Tabashnyuk <otabashniuk@outlook.com>
2020-12-14 13:03:02 +0200

    updated tasks 1-3

--- js/task-03.js
+++ js/task-03.js
@@ -1,5 +1,17 @@
-// Напиши функцию findLongestWord(string), которая принимает параметром произвольную строку (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
-// let string = 'The quick brown fox jumped over the lazy dog';
+// Напиши фукцнию findLongestWord(string), которая принимает параметром произвольную строку (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
+
+// const findLongestWord = function(string) {
+//   // твой код
+// };
+
+// /*
+//  * Вызовы функции для проверки работоспособности твоей реализации.
+//  */
+// console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+// console.log(findLongestWord('Google do a roll')); // 'Google'
+
+// console.log(findLongestWord('May the force be with you')); // 'force'
 
 const findLongestWord = function (string) {
   const array = string.split(' ');
